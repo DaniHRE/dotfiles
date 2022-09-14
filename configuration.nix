b7334691b5708b0c51e37f9445ff39cf5888027b
Daniel Henrique <henriqueevaldo@outlook.com>
2022-09-14 07:52:04 -0300

    kitty terminal added

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
 
 { config, pkgs, lib, ... }:
 
-# SET HOMA-MANAGER REPOSITORY TO INSTAL
+# SET HOME-MANAGER REPOSITORY TO INSTAL
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz";
 in {
@@ -13,11 +13,51 @@ in {
       ./hardware-configuration.nix
       (import "${home-manager}/nixos")
     ];
-  
+
   # HOME MANAGER CONFIG
   home-manager.users.dino = {
       /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
       home.packages = with pkgs; [ cbonsai tree ];
+
+      # HOME-MANAGER VARIABLES
+      home.sessionVariables = {
+        TERMINAL="kitty";
+      };
+
+      # KITTY TERMINAL CONFIG
+      programs.kitty = {
+        enable = true;
+        settings = {
+          font_size = "16.0";
+          font_family      = "FiraCode Nerd Font";
+          bold_font        = "auto";
+          italic_font      = "auto";
+          bold_italic_font = "auto";
+          background = "#001e26";
+          foreground = "#708183";
+          selection_foreground ="#93a1a1";
+          selection_background = "#002b36";
+          cursor = "#708183";
+
+          color0  = "#002731";
+          color1  = "#d01b24";
+          color2  = "#728905";
+          color3  = "#a57705";
+          color4  = "#2075c7";
+          color5  = "#c61b6e";
+          color6  = "#259185";
+          color7  = "#e9e2cb";
+          color8  = "#001e26";
+          color9  = "#bd3612";
+          color10 = "#465a61";
+          color11 = "#52676f";
+          color12 = "#708183";
+          color13 = "#5856b9";
+          color14 = "#81908f";
+          color15 = "#fcf4dc";
+
+        };
+      };
 
       # GIT CONFIG
       programs.git = {
