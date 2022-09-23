8e771cbfbc12a944ebf2ef5e006d981b9e49d955
Daniel Henrique <henriqueevaldo@outlook.com>
2022-09-23 11:07:19 -0300

    back fonts config to main config file ↺

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,31 @@
   environment.shells = with pkgs; [ zsh ];
   users.users.dino.shell = pkgs.zsh;
 
+  # FONTS
+  fonts = {
+    fonts = with pkgs; [
+      # Regular fonts
+      (nerdfonts.override {
+        fonts = [ "FiraCode" "DroidSansMono" "FiraMono" ];
+      })
+      dejavu_fonts
+      font-awesome
+
+      # Japanese fonts
+      rictydiminished-with-firacode
+      hanazono
+      ipafont
+      kochi-substitute
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "DejaVu Sans Mono" "IPAGothic" ];
+        sansSerif = [ "DejaVu Sans" "IPAPGothic" ];
+        serif = [ "DejaVu Serif" "IPAPMincho" ];
+      };
+    };
+  };
+
   # ALLOW SOFTWARE WITH UNFREE LICENSE
   nixpkgs.config.allowUnfree = true;
 
