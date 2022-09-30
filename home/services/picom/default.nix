5bb2922d7621b114ee77d3a4b863bdac853f59ae
Daniel Henrique <henriqueevaldo@outlook.com>
2022-09-30 09:45:09 -0300

    added picom composer 📝

---
+++ home/services/picom/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  services.picom = {
+    enable = true;
+    activeOpacity = 1.0;
+    inactiveOpacity = 0.8;
+    backend = "glx";
+    fade = true;
+    fadeDelta = 5;
+    opacityRules = [ "100:name *= 'i3lock'" ];
+    shadow = true;
+    shadowOpacity = 0.75;
+  };
+}
